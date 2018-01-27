7e49b65adc3175377de9d74a9030e4a38b09b432
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-01-27 16:33:37 +0200

    Add support of gitlab and bitbucket

--- loader.js
+++ loader.js
@@ -1,28 +1,33 @@
-var download = require('download-github-repo')
+var download = require('download-git-repo')
 
 var DEFAULT_SERVICE = 'github'
-var repositoryRegexp = /^[-_\.\/\w]+(#[^^~:\s\\]+)?$/
+var repositoryRegexp = /^[-_\.\w]+\/[-_\.\/\w]*[-_\.\w]+(#[^^~:\s\\]+)?$/
+
+function load (service, repository, dest) {
+  return new Promise(function (resolve, reject) {
+    console.log('(git) loading sources...')
+
+    download(service + ':' + repository, dest, function (err) {
+      if (err) reject(err)
+      else resolve({ path: dest })
+    })
+  })
+}
 
 var services = {
-  github: function loadGithubRepository (repository, dest) {
-    return new Promise(function (resolve, reject) {
-      console.log('(git) loading sources...')
-
-      download(repository, dest, function (err) {
-        if (err) reject(err.toString())
-        else resolve({ path: dest })
-      })
-    })
-  }
+  github: load.bind(null, 'github'),
+  bitbucket: load.bind(null, 'bitbucket'),
+  gitlab: load.bind(null, 'gitlab')
 }
 
 function isValidRepository (repository) {
   if (!repositoryRegexp.test(repository)) return false
-  if (!~repository.indexOf('//')) return false
+  if (~repository.indexOf('//')) return false
   return true
 }
 
-function resolveSource (source) {
+function resolveService (source, argv) {
+  source = source || ''
   var sep = source.indexOf(':')
   var service = ~sep ? source.slice(0, sep) : DEFAULT_SERVICE
   var repository = ~sep ? source.slice(sep + 1) : source
@@ -35,11 +40,12 @@ function resolveSource (source) {
     throw '(git) repository name is required'
   }
 
-  if (isValidRepository(repository)) {
+  if (!isValidRepository(repository)) {
     throw '(git) invalid repository name'
   }
 
   return {
+    name: service,
     service: services[service],
     repository: repository
   }
@@ -47,19 +53,21 @@ function resolveSource (source) {
 
 
 function loadGitRepository (source, argv) {
+  console.log({ argv, source })
   try {
-    source = resolveSource(source)
+    source = resolveService(source, argv)
   } catch (error) {
     return Promise.reject(error)
   }
 
   var service = source.service
+  var serviceName = source.name
   var repository = source.repository
   var dest = argv._[1] || source.repository.split('/')[1]
 
   return service(repository, dest)
     .catch(function (error) {
-      throw '(git) can not load ' + repository + ' due to:\n  ' + error.toString()
+      throw '(git) can not load ' + serviceName + ':' + repository + ' due to:\n  ' + error.toString()
     })
 }
 
